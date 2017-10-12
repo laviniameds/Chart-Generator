59470edbe0436083be79803e87919d8691dc2251
Lavinia Medeiros <laviniameds@icloud.com>
2017-10-12 20:31:02 -0300

    -lendo arquivo csv; -gerando array com quantidades por bairro e total;

--- src/File.java
+++ src/File.java
@@ -1,3 +1,65 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class File {
+	
+    private String csvFile = "/home/lavinia/workspace/Chart-Generator/dataset/airbnb.csv";
+    private BufferedReader br = null;
+    private String line = "";
+    private String cvsSplitBy = ",";
+    private int case1 = 0, case2 = 0, case3 = 0, case4 = 0;
+
+
+    public int[] ofertasBairro() {
+    	
+    	int valoresBairros[] = new int[5];
+        try {
+
+            br = new BufferedReader(new FileReader(csvFile));
+            while ((line = br.readLine()) != null) {
+
+                String[] linhaArquivo = line.split(cvsSplitBy);
+                for(int i = 0;i<linhaArquivo.length;i++){
+                	switch(linhaArquivo[i].toString()){
+	                	case "Dn Laoghaire-Rathdown": case1++;
+	                		break;
+	                	case "Dublin City": case2++;
+	                		break;
+	                	case "Fingal": case3++;
+	                		break;
+	                	case "South Dublin": case4++;
+	                		break;
+	                	default: 
+	                		break;
+                	}
+                }
+                
+            }
+            valoresBairros[0] = case1;
+            valoresBairros[1] = case2;
+            valoresBairros[2] = case3;
+            valoresBairros[3] = case4;
+            valoresBairros[4] = case1 + case2 + case3 + case4;
+
+        } 
+        
+        catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+		return valoresBairros;
+
+    }
 
 }
